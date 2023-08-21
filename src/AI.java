e7162ed082169d09f0b7e88db991aea0af75effc
Tasfia Tabassum <58984231+TasfiaTabassum@users.noreply.github.com>
2023-08-21 19:47:56 +0600

    Delete AI.java

--- src/AI.java
+++
@@ -1,7 +0,0 @@
-public class AI {
-    String ai1 = "Asia Based Automated Driving AI";
-    String ai2 = "USA Based Automated Driving AI";
-
-    // Customer can NOT choose
-    // It is based on GEOGRAPHICAL AREA
-}
