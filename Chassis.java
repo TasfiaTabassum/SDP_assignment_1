bd279b5f46a9a9ad03a1cf0aff749bbf857ea77e
Muntaher25 <72722460+Muntaher25@users.noreply.github.com>
2023-08-21 21:10:18 +0600

    CHassis file is changed

--- Chassis.java
+++ Chassis.java
@@ -1,4 +1,4 @@
-interface Chassis {
+interface chassis {
     String chesis1 = "Tabular";
     String chesis2 = "Backbone";
     String chesis3 = "Ladder Frame";
@@ -10,7 +10,7 @@ interface Chassis {
 }
 
 // Here the chassistype1 is configured
-class type1 implements Chassis{
+class type1 implements chassis{
 
     public String ChassisType(){
         return "Tabular";
@@ -23,7 +23,7 @@ class type1 implements Chassis{
 }
 
 // Here the chassistype2 is configured
-class type2 implements Chassis{
+class type2 implements chassis{
 
     public String ChassisType(){
         return "Backbone";
@@ -36,7 +36,7 @@ class type2 implements Chassis{
 }
 
 // Here the chassistype3 is configured
-class type3 implements Chassis{
+class type3 implements chassis{
 
     public String ChassisType(){
         return "Ladder frame";
@@ -46,4 +46,18 @@ class type3 implements Chassis{
         return 3;
     }
 
+}
+
+public class Chassis{
+    public static void main(String[] args){
+
+        chassis ch1 = new type1(); 
+        chassis ch2 = new type2(); 
+        chassis ch3 = new type3(); 
+       
+         
+        System.out.println("Enter the name of the Chassis type1: "+ ch1+ "Enter the value: "+ ch1.value());
+        System.out.println("Enter the name of the Chassis type2: "+ ch2+ "Enter the value: "+ ch2.value());
+        System.out.println("Enter the name of the Chassis type3: "+ ch3+ "Enter the value: "+ ch3.value());
+    } 
 }
