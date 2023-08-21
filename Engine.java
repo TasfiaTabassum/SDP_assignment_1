35bdb7978e1b3e295f4a7351f11ea73217f0c2e1
Muntaher25 <72722460+Muntaher25@users.noreply.github.com>
2023-08-21 20:25:56 +0600

    Add files via upload
    New features are being added

--- Engine.java
+++ Engine.java
@@ -1,5 +1,7 @@
 // Here we used interface as it helps us to use multiple inheritance
-interface Engine {
+// Here strategy pattern is used
+
+interface engine {
 
     String engineName();
     int value();
@@ -7,7 +9,7 @@ interface Engine {
 }
 
 // here we implement the 1300CC  Engine configuration
-class Engine1 implements Engine{
+class Engine1 implements engine{
 
     @Override
     public String engineName()
@@ -24,7 +26,7 @@ class Engine1 implements Engine{
 
 
 // here we implement the 1700CC  Engine configuration
-class Engine2 implements Engine{
+class Engine2 implements engine{
 
     @Override
     public String engineName()
@@ -40,7 +42,7 @@ class Engine2 implements Engine{
 }
 
 // here we implement the 1800CC  Engine configuration
-class Engine3 implements Engine{
+class Engine3 implements engine{
 
     @Override
     public String engineName()
@@ -56,7 +58,7 @@ class Engine3 implements Engine{
 }
 
 // here we implement the 2100CC  Engine configuration
-class Engine4 implements Engine{
+class Engine4 implements engine{
 
     @Override
     public String engineName()
@@ -67,6 +69,28 @@ class Engine4 implements Engine{
     public int value()
     {
        return 2100;
+    }
+
+}
+
+public class Engine{
+    public static void main(String[] args){
+        engine engine1 = new Engine1();
+        engine engine2 = new Engine2();
+        engine engine3 = new Engine3();
+        engine engine4 = new Engine4();
+
+        System.out.println("Enter the name of the Engine1: "+ engine1+ "Enter the value: "+ engine1.value());
+        System.out.println("Enter the name of the Engine2: "+ engine1+ "Enter the value: "+ engine2.value());
+        System.out.println("Enter the name of the Engine3: "+ engine1+ "Enter the value: "+ engine3.value());
+        System.out.println("Enter the name of the Engine4: "+ engine1+ "Enter the value: "+ engine4.value());
+
+
+
+
+
+
+
     }
 
 }
