e46460757171a6111a93426bb858ad2d8b85a43e
Tasfia Tabassum <58984231+TasfiaTabassum@users.noreply.github.com>
2023-09-06 09:23:58 +0600

    Add files via upload

--- src/openRoof.java
+++ src/openRoof.java
@@ -1,55 +1,13 @@
-// component interface
-interface openroof{
-    String getDescription();
-    int val();
-}
-
-class roof implements openroof{
-
-    @Override
-    public String getDescription(){
-        return "YES/NO";
-    }
-
-    @Override
-    public int val(){
-        return 0;
-    }
-}
-
-// Decorator interface
-interface CarDecorator extends Car {
-    // Additional methods for customizing/decorating
-}
-
-// Concrete Decorators (Customization Options)
-class CustomizedOpenroofdDecorator implements CarDecorator {
-    private Car car;
-    private double cost;
-
-    public CustomizedOpenroofdDecorator(Car car, double cost) {
+public class openRoof extends CarDecorator{
+    public OpenRoofDecor(Car car) {
         this.car = car;
-        this.cost = cost;
     }
-
     @Override
-    public String getDescription() {
-        return car.getDescription() + ", Customized roof sytem";
+    public double getCarPrice() {
+        return car.getCarPrice() + 2500;
     }
-
     @Override
-    public double getCost() {
-        return car.getCost() + cost;
-    }
-}
-
-public class openRoof{
-    public static void main(String[] args) {
-        Car basicCar = new BasicCar();
-
-        Car customizedCar = new CustomizedRainShieldDecorator(basicCar, 500);
-        System.out.println("Customized Car: " + customizedCar.getDescription() + " - Cost: $" + customizedCar.getCost());
-
-        
+    public String getCarDetails() {
+        return car.getCarDetails() + "\nOpen Roof";
     }
 }
